678c1cccc7a0ae73e04d576b03c64ee9298072c2
古理嶺 <klymene65@gmail.com>
2024-01-03 13:05:58 +0900

    security.txtを更新

--- src/routes/.well-known/security.txt/+server.ts
+++ src/routes/.well-known/security.txt/+server.ts
@@ -3,7 +3,7 @@ import { authorURL, siteDomain, siteRepo } from "$lib/site.config"
 export function GET() {
     const resp = `Contact: https://github.com/${siteRepo}/security
 
-Expires: 2023-12-31T23:59:59+09:00
+Expires: 2024-12-31T23:59:59+09:00
 
 Preferred-Languages: ja, en
 
